58ca952267f742d551ec57f24bb9ad5a3fbcf07e
sulli <sullivan.atatri@gmail.com>
2019-11-03 18:23:56 +0100

    Add basic docker configuration & local wp config sample file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# works
+# Works
---
+++ wordpress/config/wp-config-sample.php
@@ -0,0 +1,21 @@
+/** BaseUrl & Core url  */
+define('WP_HOME', 'http://0.0.0.0:9999');
+define('WP_SITEURL', 'http://0.0.0.0:9999');
+
+/** MySQL database name */
+define('DB_NAME', 'wp');
+
+/** MySQL database username */
+define('DB_USER', 'wp');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'wp');
+
+/** MySQL hostname */
+define('DB_HOST', 'mysql');
+
+/** Debug mode */
+define('WP_DEBUG', true);
+
+/** Caching */
+define( 'WP_CACHE', false );
